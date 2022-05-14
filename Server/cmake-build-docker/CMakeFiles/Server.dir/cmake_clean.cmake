038d273437b4636a971d44b38c1d8fa3f56624fc
Schergerat <92033428+Schergerat@users.noreply.github.com>
2022-05-14 17:29:00 +0200

    Add files via upload

---
+++ Server/cmake-build-docker/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/main.c.o"
+  "CMakeFiles/Server.dir/keyValStore.c.o"
+  "CMakeFiles/Server.dir/sub.c.o"
+  "Server.pdb"
+  "Server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
